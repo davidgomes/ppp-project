f92ba07a26b665823d0b7d1769c7b89467fe6090
David Gomes <davidrafagomes@gmail.com>
2014-04-29 13:46:55 +0100

    Initial commit, with CMake and hello world C source file.

---
+++ build/CMakeFiles/ppp-project.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/Users/david/src/ppp-project/src/main.c" "/Users/david/src/ppp-project/build/CMakeFiles/ppp-project.dir/src/main.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../PLATFORMER_INCLUDES"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/CMakeFiles/ppp-project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ppp-project.dir/src/main.c.o"
+  "ppp-project.pdb"
+  "ppp-project"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/ppp-project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
