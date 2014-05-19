5790b394b74b66b016a4dbf5bdc1472137a716d9
David Gomes <davidrafagomes@gmail.com>
2014-05-19 16:37:05 +0100

    Fixed date comparison.

--- src/xtime.c
+++ src/xtime.c
@@ -2,32 +2,50 @@
 
 int xtime_smaller(xtime *a, xtime *b) // is a smaller than b
 {
-  printf("hi\n");
-
   if (a->year < b->year)
   {
     return 1;
   }
-printf("hi2\n");
+  else if (a->year > b->year)
+  {
+    return 0;
+  }
+
   if (a->month < b->month)
   {
     return 1;
   }
-printf("hi3\n");
+  else if (a->month > b->month)
+  {
+    return 0;
+  }
+
   if (a->day < b->day)
   {
     return 1;
   }
-printf("hi4\n");
+  else if (a->day > b->day)
+  {
+    return 0;
+  }
+
   if (a->hour < b->hour)
   {
     return 1;
   }
-printf("hi5\n");
+  else if (a->hour > b->hour)
+  {
+    return 0;
+  }
+
   if (a->minute < b->minute)
   {
     return 1;
   }
-printf("hi6\n");
+  else if (a->minute > b->minute)
+  {
+    return 0;
+  }
+
   return 0;
 }
