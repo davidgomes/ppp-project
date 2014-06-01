ed44b16c026611bdf788708fde7c0a6f5e4cd1b7
David Gomes <davidrafagomes@gmail.com>
2014-06-01 23:06:34 +0100

    Fixes color defines on Windows.

--- include/colors.h
+++ include/colors.h
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
-#define COLOR_YELLOW "\x1B[33m"
-#define COLOR_GREEN "\x1B[32m"
-#define COLOR_BLUE "\x1B[34m"
-#define COLOR_CYAN "\x1B[36m"
+#ifdef WINDOWS
+  #define COLOR_YELLOW ""
+  #define COLOR_GREEN ""
+  #define COLOR_BLUE ""
+  #define COLOR_CYAN ""
 
-#define COLOR_WHITE "\x1B[37m"
+  #define COLOR_WHITE ""
+#else
+  #define COLOR_YELLOW "\x1B[33m"
+  #define COLOR_GREEN "\x1B[32m"
+  #define COLOR_BLUE "\x1B[34m"
+  #define COLOR_CYAN "\x1B[36m"
+
+  #define COLOR_WHITE "\x1B[37m"
+#endif
 
 /**
  * Reinicia a cor a branco.
